3a34b7446bb2453410a9fd6e255dfb06f6bc1141
Ekaterina1971 <ivanova71dom@yandex.ru>
2024-08-14 15:02:18 +0300

    задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // задача №1
+        System.out.println(" Задача №1");
+        int vklad = 15000;
+        int summa = 0;
+        int i = 0;
+        while ( summa <= 2459000) {
+            summa = summa + summa / 100;
+            summa = summa + vklad;
+            i = i+1;
+            System.out.println("Месяц " + i + " Итого " + summa);
+            }
+        }
     }
-}
